c5995d4a752c6568222b3dd0b8316e4666df6c5c
a132395 <67187730+a132395@users.noreply.github.com>
2020-08-11 10:01:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "$RCLONE_CONFIG" > rclone.conf
+	echo "on-download-stop=./delete.sh" >> aria2c.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
 	chmod +x on-complete.sh
+	chmod +x delete.sh
 fi
 
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
